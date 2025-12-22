d61688f287f6c3be67337141e2d494ebe44d8dad
M.Rieck <mr@number10.de>
2025-12-22 16:52:59 +0100

    fix(vdom): vdom patch to preserve parent reference

--- packages/ui/src/vdom.ts
+++ packages/ui/src/vdom.ts
@@ -1282,6 +1282,7 @@ export function patchVNode(parent: ParentType, oldV: VNode | null, newV: VNode |
   }
   const nodeType = oldV.type as NodeType
   newV = setVNodePropSafe(newV, '__node', oldV.__node)
+  newV = setVNodePropSafe(newV, '__parent', oldV.__parent ?? parent)
 
   // Transfer theme from newV to oldV (theme may have changed)
   // Transfer theme from newV to oldV, or inherit from oldV if newV has no theme
