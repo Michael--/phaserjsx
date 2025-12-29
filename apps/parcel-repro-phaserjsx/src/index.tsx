e86ffd48784180f30dfb260c96e8f71f172f6803
M.Rieck <mr@number10.de>
2025-12-29 18:21:59 +0100

    chore: adjust parcel app

--- apps/parcel-repro-phaserjsx/src/index.tsx
+++ apps/parcel-repro-phaserjsx/src/index.tsx
@@ -1,9 +1,16 @@
 /** @jsx createElement */
 import './styles.css'
 
-import { Button, Text, createElement, createPhaserJSXPlugin } from '@number10/phaserjsx'
+import {
+  Button,
+  MountProps,
+  Text,
+  View,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  createElement,
+  createPhaserJSXPlugin,
+} from '@number10/phaserjsx'
 import * as Phaser from 'phaser'
-//;(globalThis as any).Phaser = Phaser
 
 class MainScene extends Phaser.Scene {
   preload() {
@@ -12,23 +19,48 @@ class MainScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, 'logo')
-    this.add.text(20, 20, 'Phaser OK', { fontSize: '24px', color: '#fff' })
   }
 }
 
-function Test42() {
+export interface AppProps extends MountProps {
+  /** Additional props can be defined here if needed */
+  title: string
+}
+
+function Test42(props: AppProps) {
   return (
-    <Button>
-      <Text text="Hello" />
-    </Button>
+    <View
+      width={'fill'}
+      height={'fill'}
+      alignItems="center"
+      justifyContent="center"
+      gap={10}
+      backgroundColor={0x775522}
+    >
+      <Button variant="primary" size="large" onClick={() => alert('Hello from PhaserJSX!')}>
+        <Text text={props.title} />
+      </Button>
+    </View>
   )
 }
 
 new Phaser.Game({
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  scene: MainScene,
+  width: '100%',
+  height: '100%',
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    zoom: 1,
+  },
+  input: {
+    mouse: true,
+    touch: true,
+    activePointers: 2,
+  },
+  disableContextMenu: true,
+  backgroundColor: '#2a2a2a',
+  scene: [MainScene],
   plugins: {
     global: [
       createPhaserJSXPlugin({
